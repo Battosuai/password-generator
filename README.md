b66f220f29e9b23dc06591394501179e48e216cd
Battosuai <imdjiban@gmail.com>
2021-08-16 15:38:11 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+> #Simple Password Generator
+
+> ## To run, "npm start" or "node index.js"
+
+> ## Options:
+
+- "-l/--length" for password length
+- "-s/--save" to save password in a text file
+- "-nn/-no-numbers" to remove numbers from password
+- "-ns/-no-symbols" to remove symbols from passowrd
+- "-h/--help" for options
+
+> ## To use "npm link" add the path in index.js
---
+++ index.js
@@ -0,0 +1,33 @@
+#!C:\Program Files\nodejs\ node
+const program = require("commander");
+const chalk = require("chalk");
+const clipboardy = require("clipboardy");
+const log = console.log;
+const createPassword = require("./utils/createPassword");
+const savePassword = require("./utils/savePassword");
+
+program.version("1.0.0").description("Simple Password Generator");
+program
+    // .command("generate")
+  //   .action(() => {
+  //     console.log("generated");
+  //   })
+  .option("-l, --length <number>", "Length of password", "8")
+  .option("-s, --save", "Save password to passwords.txt")
+  .option("-nn, --no-numbers", "Remove numbers")
+  .option("-ns, --no-symbols", "Remove symbols")
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+const generatedPassword = createPassword(length, numbers, symbols);
+
+if (save) {
+  savePassword(generatedPassword);
+}
+
+clipboardy.writeSync(generatedPassword);
+
+log(chalk.blue("Generated Password: ") + chalk.bold(generatedPassword));
+
+log(chalk.yellow("Password copied to clipboard"));
